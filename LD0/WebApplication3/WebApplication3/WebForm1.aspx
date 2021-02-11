495741068ed13ce4ec50a66a07143eb1c4de6e75
evaldas1994 <evaldas.tuleikis@gmail.com>
2021-02-11 06:44:56 +0200

    Sesijos naudojimas būsenai saugoti

---
+++ LD0/WebApplication3/WebApplication3/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Naujas įrašas:"></asp:Label><br />
+            <asp:TextBox ID="TextBox1" runat="server" Width="400px"></asp:TextBox><br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Įvesti" OnClick="Button1_Click" /><br />
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Egzistuojantys įrašai:"></asp:Label><br />
+            <asp:Table ID="Table1" runat="server" BorderColor="Black" BorderWidth="1px" Height="70px" Width="400px">
+            </asp:Table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ LD0/WebApplication3/WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private string issaugotasTekstas;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Paimame issaugota reiksme is sesijos kintamojo
+            issaugotasTekstas = (string)Session["tekstas"];
+
+            IterptiIrasa(issaugotasTekstas);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            issaugotasTekstas = TextBox1.Text;
+            IterptiIrasa(issaugotasTekstas);
+
+            // issaugome reiksme sesijos kintamajame
+            Session["tekstas"] = issaugotasTekstas;
+        }
+
+        private void IterptiIrasa(string tekstas)
+        {
+            TableCell cell = new TableCell();
+            cell.Text = tekstas;
+
+            TableRow row = new TableRow();
+            row.Cells.Add(cell);
+
+            Table1.Rows.Add(row);
+        }
+    }
+}
