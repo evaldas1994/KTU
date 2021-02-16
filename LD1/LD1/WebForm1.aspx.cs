eca86efbf4fe0b29519453f67f52dbf214b5bce8
evaldas1994 <evaldas.tuleikis@gmail.com>
2021-02-16 17:44:01 +0200

    int[] GetNearestFlowerShop() (done)

--- LD1/LD1/WebForm1.aspx.cs
+++ LD1/LD1/WebForm1.aspx.cs
@@ -22,6 +22,8 @@ namespace LD1
             List<City> cities = new List<City>();
             ReadFile(pathD, cities);
 
+            int[] nearestShop = GetNearestFlowerShop(cities);
+
             WriteFile(pathR, cities);
         }
 
@@ -99,6 +101,45 @@ namespace LD1
                 Console.WriteLine("The file could not be wrote");
                 Console.WriteLine(ee.Message);
             }
+        }
+
+        public int[] GetNearestFlowerShop(List<City> cities)
+        {
+            int[] nearestFlowerShop = new int[] { -1, -1 };
+            for (int i=0;i<cities[0].Map.Length; i++)
+            {
+                for(int j=0; j < cities[0].Map.Length; j++)
+                {
+                    if(cities[0].Map[i][j] == 'G')
+                    {
+                        if(nearestFlowerShop[0] < 0)
+                        {
+                            nearestFlowerShop[0] = i;
+                            nearestFlowerShop[1] = j;
+                        }
+                        else
+                        {
+                            if (GetDistanceToNearestShop(cities, nearestFlowerShop) > GetDistanceToNewShop(cities, i, j))
+                            {
+                                nearestFlowerShop[0] = i;
+                                nearestFlowerShop[1] = j;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return nearestFlowerShop;
+        }
+
+        public int GetDistanceToNearestShop(List<City> cities, int[] nearestFlowerShop)
+        {
+            return ((cities[0].Location[0] - 1) - nearestFlowerShop[0]) + ((cities[0].Location[1] - 1) - nearestFlowerShop[1]);
+        }
+
+        public int GetDistanceToNewShop(List<City> cities, int i, int j)
+        {
+            return ((cities[0].Location[0] - 1) - i) + ((cities[0].Location[1] - 1) - j);
         }
     }
 }
