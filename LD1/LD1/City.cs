a954be5c280da9c438ece2f2615453fbc0085118
evaldas1994 <evaldas.tuleikis@gmail.com>
2021-02-16 08:36:12 +0200

    add button

---
+++ LD1/LD1/City.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LD1
+{
+    public class City
+    {
+        private char[] street;
+
+        public City()
+        {
+            this.Street = street;
+        }
+
+        public char[] Street
+        {
+            get; set;
+        }
+
+    }
+}
--- LD1/LD1/WebForm1.aspx.cs
+++ LD1/LD1/WebForm1.aspx.cs
@@ -13,5 +13,10 @@ namespace LD1
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
